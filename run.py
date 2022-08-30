ccb60da9faf7fd69194a2ce92b48ddb03e407245
Yi Li <yi1.li@intel.com>
2022-08-30 22:54:55 +0800

    add yiceda demo to run.py
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- run.py
+++ run.py
@@ -3,27 +3,20 @@ import multiprocessing
 import time
 import yiceda
 import schedule
-# from apscheduler.schedulers.blocking import BlockingScheduler
 
 def run():
   print('start report ...')
-  #归位
   yiceda.report_health_info()
 
-schedule.every().day.at("15:07").do(run)
-schedule.every().minute.at(':30').do(run)
-schedule.every().minute.at(':01').do(run)
+schedule.every().day.at("06:20").do(run)
 
 def schedule_run():
   while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(300)
 
 if __name__=='__main__':
   print('welcome!') 
-  # sched = BlockingScheduler()
-  # sched.add_job(run, 'cron', day_of_week='mon-fri', hour=6, minute=30, end_date='2022-08-31')
-  # sched.start()
   p1 = multiprocessing.Process(target=schedule_run,args=())
   p1.start() 
   print('press space to exit!') 
--- yiceda.py
+++ yiceda.py
@@ -1,19 +1,55 @@
 import pyautogui
 import base
-import json
-import pyperclip
+import time
+# import json
+# import pyperclip
 
-x ,y = base.get_initial_position('wechatauto.txt - Notepad')
-field_data = {}
-def report_health_info():
-    with open("./JsonFormat.json", encoding="utf-8") as fn1: #json file path
-        field_data = json.load(fn1)
-    print(field_data)
+x ,y = base.get_initial_position('疫测达')
+# field_data = {}
 
-    pyautogui.moveTo(25+x, 65+y, 0.5)
+def submit_once_student(count):
+    # go to '复学申生康'
+    pyautogui.moveTo(300+x, 330+y, 1)
     pyautogui.click()
+    time.sleep(5)
+    # go to '非本人'
+    pyautogui.moveTo(340+x, 70+y, 1)
+    pyautogui.click()
+    # go to '信息选择'
+    pyautogui.moveTo(390+x, 115+y, 1)
+    pyautogui.click()
+    # go to '姓名'
+    while count > 0:
+        count = count - 1
+        pyautogui.moveTo(200+x, 550+y, 0.5)
+        pyautogui.dragTo(200+x, 515+y, 0.5)
+    # go to '确定'
+    pyautogui.moveTo(380+x, 415+y, 1)
+    pyautogui.click()
+    # go to '下一步'
+    pyautogui.moveTo(210+x, 745+y, 1)
+    pyautogui.click()
+    time.sleep(2)
+    # go to '用户须知'
+    pyautogui.moveTo(25+x, 550+y, 1)
+    pyautogui.click() 
+    # go to '确认提交'
+    pyautogui.moveTo(210+x, 745+y, 1)
+    pyautogui.click()
+    time.sleep(2)
+    # go to '首页'
+    # pyautogui.moveTo(210+x, 315+y, 1)
+    # pyautogui.click()
+    # time.sleep(2)
 
-    pyperclip.copy(field_data['Name'])
-    pyautogui.hotkey('ctrl', 'v')
+def report_health_info(): 
+    # with open("./JsonFormat.json", encoding="utf-8") as fn1: #json file path
+    #     field_data = json.load(fn1)
+    # print(field_data)
 
-    pyautogui.typewrite(field_data['Identity'], 0.2)
+    # pyperclip.copy(field_data['Name'])
+    # pyautogui.hotkey('ctrl', 'v')
+    submit_once_student(0)
+    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    # submit_once_student(1)
+    # pyautogui.typewrite(field_data['Identity'], 0.2)
