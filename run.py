bf3f9cd7e7ed78cbc0dc65ad71a2d280297dc3d0
Yi Li <yi1.li@intel.com>
2022-09-01 20:54:17 +0800

    yiceda: add test case
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- run.py
+++ run.py
@@ -9,14 +9,19 @@ def run():
   yiceda.report_health_info()
 
 schedule.every().day.at("06:20").do(run)
-
+schedule.every().minute.at(":20").do(run)
+schedule.every().minute.at(":50").do(run)
 def schedule_run():
   while True:
     schedule.run_pending()
-    time.sleep(300)
+    #time.sleep(300)
+    time.sleep(1)
 
 if __name__=='__main__':
-  print('welcome!') 
+  # On Windows calling this function is necessary.
+  # On Linux/OSX it does nothing.
+  multiprocessing.freeze_support()
+  print('welcome!')
   p1 = multiprocessing.Process(target=schedule_run,args=())
   p1.start() 
   print('press space to exit!') 
